fd0a3ebf3508cddc161ff741ae6fa1debb7dc188
Usuario IOT <usuarioiot@linux-iot-015.ifrn.local>
2018-02-14 10:12:22 -0300

    Implementação do primeiro exemplo

---
+++ C/exemplo01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Digite o primeiro número:");
+    scanf("%d",&a);
+    printf("Digite o segundo número:");
+    scanf("%d",&b);
+
+    c=a+b;
+    printf("O resultado da soma é %d",c);
+
+}
