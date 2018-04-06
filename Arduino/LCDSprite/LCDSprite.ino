618629a7ed6db70f8e8067765d58e4447da1de9c
Usuario IOT <usuarioiot@linux-iot-019.ifrn.local>
2018-04-06 17:19:28 -0300

    =LCD Sprite Link"

---
+++ Arduino/LCDSprite/LCDSprite.ino
@@ -0,0 +1,17 @@
+
+#include <LiquidCrystal.h>
+
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+UltraSonicDistanceSensor sensor(9,8);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Distancia");
+}
+void loop() {
+  double distancia=sensor.measureDistanceCm();
+  lcd.setCursor(0, 1);
+  lcd.print(millis());
+}
+
