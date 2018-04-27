ba62a81049c97906e0a9b43c193b99c3c82aefb8
Usuario IOT <usuarioiot@linux-iot-004.ifrn.local>
2018-04-27 17:52:59 -0300

    Atualização código LCD e diretório da prova

--- Arduino/LCDSpriteUltrasonico/LCDSpriteUltrasonico.ino
+++ Arduino/LCDSpriteUltrasonico/LCDSpriteUltrasonico.ino
@@ -1,20 +1,22 @@
 
 #include <LiquidCrystal.h>
 
+#include <HCSR04.h>
 const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+UltraSonicDistanceSensor sensor(12,11);
 
 void setup() {
 
   lcd.begin(16, 2);
-  lcd.print("Ligou");
+  lcd.print("Distancia");
   Serial.begin(9600);
 }
 void loop() {
+  double distancia=sensor.measureDistanceCm();
   lcd.setCursor(0, 1);
-  lcd.print(millis()/1000);
+  lcd.print(distancia);
+  Serial.println(distancia);
   delay(1000);
-
 }
 
