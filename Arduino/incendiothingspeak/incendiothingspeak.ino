358cb47363573c6e0f4e0dbb324578900348bfdc
Usuario IOT <usuarioiot@linux-iot-002.ifrn.local>
2018-05-11 09:47:10 -0300

    =Comunicacao ESP8266 - sensor de chama

---
+++ Arduino/incendiothingspeak/incendiothingspeak.ino
@@ -0,0 +1,50 @@
+
+
+#include <SoftwareSerial.h>
+
+const int PINO_D0=7;
+const int TX=11;
+const int RX=10;
+const String API_KEY="xxxxxxx";
+const String HOST="api.thingspeak.com";
+const int PORT=80;
+int countTimeCommand;
+boolean found=false;
+SoftwareSerial esp8266(RX,TX);
+void setup() {
+ pinMode(PIN_A0,INPUT);
+ pinMode(PINO_D0,INPUT);
+ Serial.begin(9600);
+ esp8266.begin(115200);
+ delay(2000);
+ esp8266.println("AT");
+ delay(4000);
+ esp8266.println("AT+CMODE=1");
+ delay(4000);
+ esp8266.println("AT+CWJAP=\"wIFRN-IoT\",\"Ifrn@IoT!N0CFrZ\"");
+ delay(4000);
+}
+
+void loop() {
+ int distanciaChama=analogRead(PIN_A0);
+ int alteracao=digitalRead(PINO_D0);
+ int chama;
+ if(alteracao!=HIGH){
+    chama=1;
+ }else{
+    chama=0;
+ }
+ String comando="GET /update?api_key="+API_KEY+"&field1="+String(distanciaChama)+"&field2="+String(chama);
+ esp8266.println("AT+CIPSTART=\"TCP\",\""+HOST+"\",80");
+ delay(2000);
+ esp8266.println("AT+CIPSEND="+String(comando.length()+4));
+ delay(2000);
+ esp8266.println(comando);
+ Serial.println(comando);
+ delay(3500);
+ esp8266.println("AT+CIPCLOSE");
+ Serial.println(comando);
+ delay(10000);
+}
+
+
