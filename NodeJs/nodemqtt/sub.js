0826d41594edb6012c8b2cec25ba60fbf3d5af24
Andre Almeida <andre.almeida@escolar.ifrn.edu.br>
2018-06-12 18:44:33 -0300

    Atualização exemplo Node.JS+MQTT

--- NodeJs/nodemqtt/sub.js
+++ NodeJs/nodemqtt/sub.js
@@ -6,6 +6,10 @@ cliente.on('connect',function(){
     console.log('Conectou');
 });
 
-cliente.on('message',function(topic,message){
+cliente.on('message',function(topic,message,pacote){
     console.log(message.toString());
+    var temperatura=parseInt(message.toString());
+    if(temperatura>40){
+		console.log("Temperatura elevada");
+	}
 });
